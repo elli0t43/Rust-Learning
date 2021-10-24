033972dfba0761fc207d4872372c18ee9c43e3ff
elli0t43 <soumyadeeph43@gmail.com>
2021-10-25 00:01:18 +0530

    print.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// we can import files with "mod" keyword
+mod print;
+
+// our main function
 fn main() {
     println!("Hello, world!");
+    print::run(); // calling the run function from print.rs file
 }
