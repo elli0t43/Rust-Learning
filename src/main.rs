5a8cc47684b7e73a3d136f3f528baa9b6c5792ea
elli0t43 <Soumyadeeph43@gmail.com>
2021-11-30 16:30:17 +0530

    Added number_guessing_game code

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod contstants;
 mod print;
 mod scalar_types;
 mod variables;
+mod number_guessing_game;
 
 // our main function
 // The fn syntax declares a new function, the parentheses,
@@ -15,12 +16,13 @@ fn main() {
     println!("Hello, world!"); // do keep in mind that println! isn't a function its a macro.
     print::run(); // calling the run function from print.rs file
                   // variables::var_run();
-    scalar_types::integer_types_run();
-    scalar_types::integer_overflow_run();
-    scalar_types::float_types_run();
-    scalar_types::number_seperator_run();
-    scalar_types::boolean_types_run();
-    scalar_types::character_types_run();
-    contstants::constants_types_run();
-    contstants::shadowing_var_char_run();
+    //scalar_types::integer_types_run();
+    //scalar_types::integer_overflow_run();
+    //scalar_types::float_types_run();
+    //scalar_types::number_seperator_run();
+    //scalar_types::boolean_types_run();
+    //scalar_types::character_types_run();
+    //contstants::constants_types_run();
+    //contstants::shadowing_var_char_run();
+    number_guessing_game::number_guessing_game_run();
 }
